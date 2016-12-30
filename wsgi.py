be05c081c7e9e8732e26f502b11d508307181597
Aaron Reyes <aaronreyes2@gmail.com>
2016-12-29 18:05:44 -0600

    getting production hello world working

--- wsgi.py
+++ wsgi.py
@@ -12,7 +12,7 @@ import os
 from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
