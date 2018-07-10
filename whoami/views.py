9ab5aa96415f613bad097386de68f425c7125f0c
Aaron Reyes <aaronreyes2@gmail.com>
2018-07-09 21:28:47 -0500

    removing try/except to debug production server

--- whoami/views.py
+++ whoami/views.py
@@ -26,28 +26,28 @@ def main(request):
   info['Access-Control-Allow-Credentials'] = 'false'
   info['Access-Control-Max-Age'] = '-1'
   # get the geoname_id for this IP
-  try:
-    n = [int(x) for x in info['IP'].split('.')]
-    _ip = (n[0] * (1 << 24)) + (n[1] * (1 << 16)) + (n[2] * (1 << 8)) + n[3]
-    blk = Block.objects.filter(_ipstart__lte=_ip, _ipend__gte=_ip).values().first()
-    asn = Asn.objects.filter(_ipstart__lte=_ip, _ipend__gte=_ip).values().first()
-    loc = Location.objects.filter(geoname_id=blk['geoname_id']).values().first()
-    # cleanup response
-    blk.pop('id', None)
-    blk.pop('_ipend', None)
-    blk.pop('_ipstart', None)
-    blk.pop('geoname_id', None)
-    asn.pop('id', None)
-    asn.pop('_ipend', None)
-    asn.pop('_ipstart', None)
-    loc.pop('id', None)
-    loc.pop('geoname_id', None)
-    for k in blk:
-      info[k.upper()] = blk[k]
-    for k in loc:
-      info[k.upper()] = loc[k]
-    for k in asn:
-      info[k.upper()] = asn[k]
-  except:
-    pass # ignore failures
+  # try:
+  n = [int(x) for x in info['IP'].split('.')]
+  _ip = (n[0] * (1 << 24)) + (n[1] * (1 << 16)) + (n[2] * (1 << 8)) + n[3]
+  blk = Block.objects.filter(_ipstart__lte=_ip, _ipend__gte=_ip).values().first()
+  asn = Asn.objects.filter(_ipstart__lte=_ip, _ipend__gte=_ip).values().first()
+  loc = Location.objects.filter(geoname_id=blk['geoname_id']).values().first()
+  # cleanup response
+  blk.pop('id', None)
+  blk.pop('_ipend', None)
+  blk.pop('_ipstart', None)
+  blk.pop('geoname_id', None)
+  asn.pop('id', None)
+  asn.pop('_ipend', None)
+  asn.pop('_ipstart', None)
+  loc.pop('id', None)
+  loc.pop('geoname_id', None)
+  for k in blk:
+    info[k.upper()] = blk[k]
+  for k in loc:
+    info[k.upper()] = loc[k]
+  for k in asn:
+    info[k.upper()] = asn[k]
+  # except:
+  #   pass # ignore failures
   return JsonResponse(info)
